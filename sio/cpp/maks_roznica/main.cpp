bd716c54cd022c2625ef216108e6fc5d70d7ec2a
DrDubel <antek.suszko@gmail.com>
2022-09-18 13:48:04 +0200

    Maksymalna różnica liczb w tablicy.

---
+++ sio/cpp/maks_roznica/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    int ilosc_liczb;
+    cin >> ilosc_liczb;
+    int ciag_liczb[ilosc_liczb];
+    for (int i = 0; i < ilosc_liczb; i++) {
+        cin >> ciag_liczb[i];
+    }
+    sort(ciag_liczb, ciag_liczb + ilosc_liczb);
+    int roznica = ciag_liczb[ilosc_liczb-1] - ciag_liczb[0];
+    cout << roznica << endl;
+}
