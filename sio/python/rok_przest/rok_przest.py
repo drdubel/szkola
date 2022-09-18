113ce27d1bb8d458819196241c69db23a8060180
DrDubel <antek.suszko@gmail.com>
2022-09-18 14:00:00 +0200

    Ulepszona czytelność kodu.

--- sio/python/rok_przest/rok_przest.py
+++ sio/python/rok_przest/rok_przest.py
@@ -1,1 +1,10 @@
-a=int(input());print("TAK") if a%100==0 and a%400==0 or a%4==0 and a%100!=0 else print("NIE")
+def main():
+    a = int(input())
+    if a % 100 == 0 and a % 400 == 0 or a % 4 == 0 and a % 100 != 0:
+        return "TAK"
+    else:
+        return "NIE"
+
+
+if __name__ == "__main__":
+    print(main())
