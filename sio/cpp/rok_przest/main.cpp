45c0ec7e72ec9b424f68d927a065306b045ce19d
DrDubel <antek.suszko@gmail.com>
2022-09-18 13:56:32 +0200

    Sprawdza czy rok jest przestępny.

---
+++ sio/cpp/rok_przest/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int rok;
+    cin >> rok;
+    if (rok%4 == 0)
+    {
+        if (rok%100 == 0 and rok%400 != 0)
+        {
+            cout << "NIE";
+        } else
+        {
+            cout << "TAK";
+        }
+    } else
+    {
+        cout << "NIE";
+    }
+    cout << "\n";
+}
