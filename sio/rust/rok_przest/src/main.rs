36bbdfe02bbcba328ddf263066e8c9abcd0a528f
DrDubel <antek.suszko@gmail.com>
2022-09-18 15:42:12 +0200

    Sprawdza czy rok jest przestępny.

---
+++ sio/rust/rok_przest/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+
+fn czy_przest(rok: i32) {
+    if rok%100 == 0 && rok%400 == 0 || rok%4 == 0 && rok%100 != 0 {
+        println!("TAK");
+    } else {
+        println!("NIE")
+    }
+}
+
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let rok: i32 = input.trim().parse().unwrap();
+    czy_przest(rok);
+}
