1e50283a9c9398c482e012823be4c86723545b7e
DrDubel <antoni.suszko@gmail.com>
2022-09-30 19:30:51 +0200

    zadania z sio2

---
+++ sio/cpp/hipoteza_goldbacha/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+bool czy_pierwsza(int liczba) {
+	for (int i=2; i <= sqrt(liczba); i++) {
+		if (liczba%i == 0) {
+			return false;
+		}
+	}
+    return true;
+}
+
+
+int main() {
+    int ilosc_liczb;
+    int liczba;
+    cin >> ilosc_liczb;
+    for (int i=0; i < ilosc_liczb; i++) {
+        cin >> liczba;
+        int lp1 = liczba/2;
+        int lp2 = 0;
+        while (lp1 + lp2 != liczba) {
+            if (czy_pierwsza(lp1) && czy_pierwsza(liczba - lp1)) {
+                lp2 = liczba - lp1;
+                break;
+            }
+            lp1++;
+        }
+        cout << lp2 << " " << lp1 << "\n";
+    }
+}
--- sio/cpp/mecz_tenisowy/main.cpp
+++ sio/cpp/mecz_tenisowy/main.cpp
@@ -1,56 +1,10 @@
 #include <iostream>
-#include <stdlib.h>
+#include <string>
 using namespace std;
 
-string input;
-int indeks = 0;
-
-bool ktory(int minwynik) {
-    int zlicz1 = 0, zlicz2 = 0;
-    while ((zlicz1 < minwynik && zlicz2 < minwynik) || abs(zlicz1, zlicz2) < 2)
-    {
-        if (input[indeks] == 'A')
-            ++zlicz1;
-        else
-            ++zlicz2;
-        ++indeks;
-    }
-    if (zlicz1 > zlicz2)
-        return 0;
-    else
-        return 1;
-}
 
 int main() {
+    string input;
     cin >> input;
-    bool wynik;
-    int g1 = 0, g2 = 0, s1 = 0, s2 = 0;
-
-    while (max(s1, s2) < 3 && indeks < input.length()) {
-        wynik = ktory(4);
-        if (!wynik)
-            ++g1;
-        else
-            ++g2;
-        if (g1 == 6 && g2 == 6) {
-            wynik = ktory(7);
-            if (!wynik) {
-                ++s1;
-            }
-            else {
-                ++s2;
-            }
-            g1 = 0;
-            g2 = 0;
-        }
-        else if (g1 >= 6 && g1 - g2 >= 2) {
-            ++s1;
-            g1 = 0, g2 = 0;
-        }
-        else if (g2 >= 6 && g2 - g1 >= 2) {
-            ++s2;
-            g1 = 0, g2 = 0;
-        }
-    }
-    cout << ((s1 == 3) ? 'A' : 'B');
+    cout << input[input.size() - 1];
 }
