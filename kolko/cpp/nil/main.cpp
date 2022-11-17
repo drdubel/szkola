a4fd9a628798818f0ea4f772372602dd29b05183
DrDubel <antoni.suszko@gmail.com>
2022-11-17 15:10:47 +0100

    zadania

--- kolko/cpp/nil/main.cpp
+++ kolko/cpp/nil/main.cpp
@@ -1,48 +1,35 @@
 #include <iostream>
-#include <algorithm>
+#include<vector>
 using namespace std;
-#include <limits.h>
-int il_odnog;
-
-int minDistance(int dist[], bool sptSet[])
-{
-    int min = INT_MAX, min_index;
-    for (int v = 0; v < il_odnog; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-    return min_index;
-}
-
-void printSolution(int dist[])
-{
-    cout << "Vertex \t Distance from Source" << endl;
-    for (int i = 0; i < il_odnog; i++)
-        cout << i << " \t\t\t\t" << dist[i] << endl;
-}
-
-void dijkstra(int graph[il_odnog][il_odnog], int src)
-{
-    int dist[il_odnog];
-    bool sptSet[il_odnog];
-    for (int i = 0; i < il_odnog; i++)
-        dist[i] = INT_MAX, sptSet[i] = false;
-    dist[src] = 0;
-    for (int count = 0; count < il_odnog - 1; count++)
-    {
-        int u = minDistance(dist, sptSet);
-        sptSet[u] = true;
-        for (int v = 0; v < il_odnog; v++)
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
-                dist[v] = dist[u] + graph[u][v];
+constexpr int maxn=1e6+7;
+vector<int> macierz[maxn];
+int wart[maxn];
+bool odw[maxn]{};
+long long read(int v){
+    long long out=0,out1=0;
+    odw[v]=1;
+    for(auto wi : macierz[v]){
+        if(!odw[wi]){
+            out=wart[wi];
+            out+=read(wi);
+        }
+        out1=max(out,out1);
     }
-    printSolution(dist);
+    return out1;
 }
-
-int main()
-{
-    cin >> il_odnog;
-    int graph[il_odnog][il_odnog];
-    fill(graph[0], graph[0] + il_odnog * il_odnog, 0);
-    dijkstra(graph, 0);
-    return 0;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,i;
+    cin>>n;
+    for(i=1;i<=n;++i){
+        cin>>wart[i];
+    }
+    int a,b;
+    for(i=1;i<n;++i){
+        cin>>a>>b;
+        macierz[a].push_back(b);
+        macierz[b].push_back(a);
+    }
+    cout<<read(1)+wart[1];
 }
--- kolko/python/nil/main.py
+++ kolko/python/nil/main.py
@@ -1,13 +1,36 @@
-def drogosprawdzacz(il_ryb, rzeka):
-    maks_zlapane_ryby = il_ryb[1]
-    zlapane_ryby = il_ryb[1]
-    
-    return maks_zlapane_ryby
+def topologicalSortUtil(v):
+    global Stack, visited, adj
+    visited[v] = True
+
+    for i in adj[v]:
+        if not visited[i[0]]:
+            topologicalSortUtil(i[0])
+
+    Stack.append(v)
 
 
-def czy_koniec(il_ryb, rzeka, odnoga):
-    if rzeka[odnoga]:
-        czy_koniec(il_ryb, rzeka, odnoga)
+def longestPath(s):
+    global Stack, visited, adj, il_odnog
+    dist = [-(10**9) for i in range(il_odnog)]
+
+    for i in range(il_odnog):
+        if visited[i] == False:
+            topologicalSortUtil(i)
+
+    dist[s] = 0
+
+    while len(Stack) > 0:
+
+        u = Stack[-1]
+        del Stack[-1]
+
+        if dist[u] != 10**9:
+            for i in adj[u]:
+                if dist[i[0]] < dist[u] + i[1]:
+                    dist[i[0]] = dist[u] + i[1]
+
+    for i in range(il_odnog):
+        print("INF ", end="") if (dist[i] == -(10**9)) else print(dist[i], end=" ")
 
 
 def main():
@@ -17,7 +40,24 @@ def main():
     for _ in range(il_odnog - 1):
         od, do = list(map(int, input().split()))
         rzeka[od].append(do)
-    print(drogosprawdzacz(il_ryb, rzeka))
+    Stack, visited = [], [False for i in range(il_odnog + 1)]
+    adj = [[] for i in range(7)]
+
+    s = 1
+    print("Following are longest distances from source vertex ", s)
+    longestPath(s)
 
 
-main()
+if __name__ == "__main__":
+    il_odnog = int(input())
+    il_ryb = {i: koszt for i, koszt in enumerate(list(map(int, input().split())))}
+    rzeka = {i: [] for i in range(1, il_odnog + 1)}
+    for _ in range(il_odnog - 1):
+        od, do = list(map(int, input().split()))
+        rzeka[od].append(do)
+    Stack, visited = [], [False for i in range(il_odnog + 1)]
+    adj = [[] for i in range(7)]
+
+    s = 1
+    print("Following are longest distances from source vertex ", s)
+    longestPath(s)
