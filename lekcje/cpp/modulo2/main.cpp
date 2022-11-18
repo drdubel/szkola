336e34f25bbf162d588aed6fc9caf98d8b178cb1
drdubel <44920161+drdubel@users.noreply.github.com>
2022-11-18 12:56:13 +0000

    zadanie modulo2

---
+++ lekcje/cpp/modulo2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+int main() {
+    int il_liczb, modulo;
+    long long liczba;
+    cin >> il_liczb >> modulo;
+    set<long long> reszty;
+    for (int i = 0; i < il_liczb; i++) {
+        cin >> liczba;
+        reszty.insert(liczba%modulo);
+    }
+    cout << reszty.size() << "\n";
+}
