2408c490d8d0bcc33a928041be48ebb7344ef21d
DrDubel <antek.suszko@gmail.com>
2022-11-29 18:14:10 +0100

    statystyki

--- lekcje/cpp/statystyki/main.cpp
+++ lekcje/cpp/statystyki/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
 #include <regex>
+#include <string>
 
 using namespace std;
 
-bool czypalindrom(string slowo)
-{
+bool czypalindrom(string slowo) {
     for (auto &i : slowo) {
         i = tolower(i);
     }
@@ -14,8 +13,15 @@ bool czypalindrom(string slowo)
     return (rev == slowo && slowo.length() > 0);
 }
 
-int main()
-{
+int policz_wystapienia(string tekst, string wzorzec) {
+    regex slowa(wzorzec);
+    auto poczatek = sregex_iterator(tekst.begin(), tekst.end(), slowa);
+    auto koniec = sregex_iterator();
+
+    return distance(poczatek, koniec);
+}
+
+int main() {
     int il_danych;
     cin >> il_danych;
     int kolejnosc_danych[il_danych], dane[il_danych];
@@ -25,18 +31,28 @@ int main()
     string tekst;
     cin >> tekst;
     for (int i = 0; i < il_danych; i++) {
-        switch (i)
-        {
-        case 1:
-            dane[i] = regex_search(text, " ")
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
+        switch (kolejnosc_danych[i]) {
+            case 1:
+                dane[i] = policz_wystapienia(tekst, " ");
+            case 2:
+                dane[i] = policz_wystapienia(tekst, "[0-9]+");
+            case 3:
+                dane[i] = policz_wystapienia(tekst, "[a-zA-Z]+");
+            case 4:
+                dane[i] = policz_wystapienia(tekst, "([^.]*[a-zA-Z]+[^.]*\\.)");
+            case 5:
+                dane[i] = 0;
+                regex slowa("[a-zA-Z]+");
+                for (sregex_iterator it =
+                         sregex_iterator(tekst.begin(), tekst.end(), slowa);
+                     it != sregex_iterator(); it++) {
+                    smatch match;
+                    match = *it;
+                    dane[i] += czypalindrom(match.str(0));
+                }
         }
+    }
+    for (auto ilosc : dane) {
+        cout << ilosc << ' ';
     }
 }
