3d3c08dcb0dcfc3a7f130f460ffb7deeb08c7cd7
DrDubel <antek.suszko@gmail.com>
2022-12-13 21:22:06 +0100

    zmiana algorytmu

--- kolko1/cpp/literki_marka/main.cpp
+++ kolko1/cpp/literki_marka/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <unordered_set>
 
 using namespace std;
 
 int main() {
     string tekst;
     int il_roznych_liter;
-    unordered_set<char> rozne_litery;
     cin >> tekst >> il_roznych_liter;
-    for (char litera : tekst) {
-        rozne_litery.insert(litera);
-    }
-    cout <<
 }
