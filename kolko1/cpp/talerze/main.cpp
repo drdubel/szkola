4885023bc8f917030db1020c748538917b0630db
DrDubel <antek.suszko@gmail.com>
2022-12-16 16:19:37 +0100

    zmiana algorytmu

--- kolko1/cpp/talerze/main.cpp
+++ kolko1/cpp/talerze/main.cpp
@@ -7,30 +7,32 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int il_talerzy, il_polecen, liczba, usunieta, dl_stosu = 0, i;
-    vector<int>::iterator it, poczatek, koniec;
+    int il_talerzy, il_polecen, liczba, usunieta, i, x = 0;
     string operacja;
-    vector<int> stos;
+    vector<int> kolejnosc;
     cin >> il_talerzy >> il_polecen;
+    int talerze[il_talerzy];
+    for (int &talerz : talerze) {
+        talerz = -1;
+    }
     for (i = 0; i < il_polecen; i++) {
         cin >> operacja;
         if (operacja == "odloz") {
             cin >> liczba;
-            dl_stosu++;
-            stos.push_back(liczba);
+            talerze[liczba - 1] = x + 1;
+            kolejnosc.push_back(liczba - 1);
+            x++;
         } else if (operacja == "pytaj") {
             cin >> liczba;
-            poczatek = stos.begin();
-            koniec = stos.end();
-            it = find(poczatek, koniec, liczba);
-            if (it != koniec) {
-                cout << dl_stosu - (it - poczatek + 1) << "\n";
-            } else {
+            if (talerze[liczba - 1] == -1) {
                 cout << "-1\n";
+            } else {
+                cout << x - talerze[liczba - 1] << "\n";
             }
         } else if (operacja == "zdejmij") {
-            dl_stosu--;
-            stos.pop_back();
+            talerze[kolejnosc.back()] = -1;
+            kolejnosc.pop_back();
+            x--;
         }
     }
 }
