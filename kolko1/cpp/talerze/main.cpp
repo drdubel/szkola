f118d50aa2aa41e9a38143cb6ed2fada8a2514a5
drdubel <44920161+drdubel@users.noreply.github.com>
2022-12-16 13:48:45 +0000

    małe przyspieszenie

--- kolko1/cpp/talerze/main.cpp
+++ kolko1/cpp/talerze/main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int il_talerzy, il_polecen, liczba, usunieta, i;
+    int il_talerzy, il_polecen, liczba, usunieta, dl_stosu = 0, i;
+    vector<int>::iterator it, poczatek, koniec;
     string operacja;
     vector<int> stos;
     cin >> il_talerzy >> il_polecen;
@@ -15,16 +16,20 @@ int main() {
         cin >> operacja;
         if (operacja == "odloz") {
             cin >> liczba;
+            dl_stosu++;
             stos.push_back(liczba);
         } else if (operacja == "pytaj") {
             cin >> liczba;
-            auto it = find(stos.begin(), stos.end(), liczba);
-            if (it != stos.end()) {
-                cout << stos.size() - (it - stos.begin() + 1) << "\n";
+            poczatek = stos.begin();
+            koniec = stos.end();
+            it = find(poczatek, koniec, liczba);
+            if (it != koniec) {
+                cout << dl_stosu - (it - poczatek + 1) << "\n";
             } else {
                 cout << "-1\n";
             }
         } else if (operacja == "zdejmij") {
+            dl_stosu--;
             stos.pop_back();
         }
     }
