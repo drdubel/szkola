4a65abf711efa1ee8a0884e2a26ab6420b35660a
drdubel <44920161+drdubel@users.noreply.github.com>
2022-12-16 13:35:58 +0000

    zadanie

--- kolko1/cpp/literki_marka/main.cpp
+++ kolko1/cpp/literki_marka/main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
-#include <unordered_set>
+#include <map>
 
 using namespace std;
 
-void szukaj_podslowa(int wym_il_roznych_liter, string tekst) {
-    unordered_set<char> uzyte_litery;
-    char akt_litera;
-    int tyl = 1, przod = 0, akt_il_roznych_liter = 0;
-    while (przod < tekst.size()) {
-        przod++;
-        akt_litera = tekst[przod];
-        if (uzyte_litery.find(akt_litera) == uzyte_litery.end()) {
-            uzyte_litery.insert(akt_litera);
-            akt_il_roznych_liter++;
-        } else if (akt_il_roznych_liter < wym_il_roznych_liter) {
-            continue;
-        }
-    }
-}
+int litery[26];
 
 int main() {
     string tekst;
-    int wym_il_roznych_liter;
+    int wym_il_roznych_liter, il_roznych_liter = 0, koniec = 0,
+                              koniec_i = 1e6 + 1, przod_i = -1;
     cin >> tekst >> wym_il_roznych_liter;
+    for (int i = 0; i < tekst.size(); i++) {
+        while (il_roznych_liter < wym_il_roznych_liter &&
+               koniec < tekst.size()) {
+            if (litery[(int)tekst[koniec] - 97] == 0) {
+                il_roznych_liter++;
+            }
+            litery[(int)tekst[koniec] - 97]++;
+            koniec++;
+        }
+        if (il_roznych_liter == wym_il_roznych_liter and
+            (koniec_i - przod_i) > (koniec - i)) {
+            koniec_i = koniec;
+            przod_i = i;
+        }
+        litery[(int)tekst[i] - 97]--;
+        if (litery[(int)tekst[i] - 97] == 0) {
+            il_roznych_liter--;
+        }
+    }
+    if (przod_i == -1) {
+        cout << "BRAK";
+    } else {
+        cout << przod_i + 1 << ' ' << koniec_i;
+    }
 }
