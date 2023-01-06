d7554b50c4a0f884500d92ad0a1164c336cc7425
DrDubel <antek.suszko@gmail.com>
2023-01-06 17:16:06 +0100

    long long

--- kolko2/cpp/logarytm/main.cpp
+++ kolko2/cpp/logarytm/main.cpp
@@ -4,23 +4,24 @@
 
 using namespace std;
 
-long long a, c, p, i;
+long long a, c, p, akt;
+int i;
 
 int logarytm() {
     a %= p, c %= p;
     int n = sqrt(p) + 1;
 
-    int an = 1;
-    for (int i = 0; i < n; ++i) an = (an * 1ll * a) % p;
+    long long an = 1;
+    for (i = 0; i < n; ++i) an = (an * a) % p;
 
     unordered_map<int, int> wartosci;
-    for (int i = 0, akt = c; i <= n; ++i) {
+    for (i = 0, akt = c; i <= n; ++i) {
         wartosci[akt] = i;
-        akt = (akt * 1ll * a) % p;
+        akt = (akt * a) % p;
     }
 
-    for (int i = 1, akt = 1; i <= n; ++i) {
-        akt = (akt * 1ll * an) % p;
+    for (i = 1, akt = 1; i <= n; ++i) {
+        akt = (akt * an) % p;
         if (wartosci.count(akt)) {
             int ans = n * i - wartosci[akt];
             return ans;
