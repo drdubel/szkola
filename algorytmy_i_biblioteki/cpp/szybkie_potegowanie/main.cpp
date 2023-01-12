f9bb6f2d8d6acb8637bcb5eb15ca74db0eda9f38
DrDubel <antoni.suszko@gmail.com>
2023-01-12 15:41:35 +0100

    skrócona funkcja pot

--- algorytmy_i_biblioteki/cpp/szybkie_potegowanie/main.cpp
+++ algorytmy_i_biblioteki/cpp/szybkie_potegowanie/main.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-long long szybkie_potegowanie(long long liczba, long long wykladnik) {
-    if (wykladnik == 0) {
-        return 1;
-    }
-    if (wykladnik % 2 == 0) {
-        long long wynik = szybkie_potegowanie(liczba, wykladnik / 2);
-        return (wynik * wynik) % 1000000007;
-    } else {
-        long long wynik = szybkie_potegowanie(liczba, wykladnik - 1);
-        return (wynik * liczba) % 1000000007;
-    }
+typedef long long ll;
+
+ll wynik;
+
+ll pot(ll x, ll n) {
+    if (n == 0) return 1;
+    if (n % 2 == 0) return pow(pot(x, n / 2), 2);
+    else return pot(x, n - 1) * x;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
-    long long il_poteg, liczba, wykladnik;
+    ll il_poteg, x, n;
     cin >> il_poteg;
-    for (long long i = 0; i < il_poteg; i++) {
-        cin >> liczba >> wykladnik;
-        cout << szybkie_potegowanie(liczba, wykladnik) << "\n";
+    for (ll i = 0; i < il_poteg; i++) {
+        cin >> x >> n;
+        cout << pot(x, n) << "\n";
     }
 }
