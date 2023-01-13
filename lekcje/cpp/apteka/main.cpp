21765dfe0add8d05939be68474a126acac779535
drdubel <44920161+drdubel@users.noreply.github.com>
2023-01-13 12:39:56 +0000

    apteka

--- lekcje/cpp/apteka/main.cpp
+++ lekcje/cpp/apteka/main.cpp
@@ -1,18 +1,27 @@
-#include <cmath>
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main() {
-    int il_osob;
-    int pozycja = 0;
-    int wydatki = 0;
-    int i = il_osob - 1;
-    cin >> il_osob;
-    vector<int> ceny_przekupstwa(il_osob);
-    for (auto &x : ceny_przekupstwa) {
-        cin >> x;
-    }
+int pozycja = 0, cena, i;
+long long il_osob, wydatki = 0;
 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> il_osob;
+    vector<pair<int, int>> ludzie = {{0, 0}};
+    for (i = 1; i < il_osob + 1; ++i) {
+        cin >> cena;
+        ludzie.push_back({cena, i});
+    }
+    sort(ludzie.begin() + 1, ludzie.end());
+    for (i = 1; i < il_osob + 1; ++i) {
+        if (ludzie[i].second > pozycja) {
+            wydatki +=
+                (long long)(ludzie[i].second - pozycja) * ludzie[i].first;
+            pozycja = ludzie[i].second;
+        }
+    }
     cout << wydatki << "\n";
 }
