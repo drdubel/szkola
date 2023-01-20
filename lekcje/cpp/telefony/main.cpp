c49fcad9ad68b5739f97530dd72ba1cbef548d9c
drdubel <44920161+drdubel@users.noreply.github.com>
2023-01-20 12:10:03 +0000

    usunięcie zmiennej

--- lekcje/cpp/telefony/main.cpp
+++ lekcje/cpp/telefony/main.cpp
@@ -5,20 +5,18 @@
 using namespace std;
 
 const int MAXN = 1e6 + 7;
-int il_kontaktow, wartosc_w_tajtonach, numer, i;
+int il_kontaktow, numer, i;
 pair<int, int> kontakty[MAXN];
 
 int main() {
     cin >> il_kontaktow;
     for (i = 0; i < il_kontaktow; ++i) {
-        wartosc_w_tajtonach = 0;
         cin >> kontakty[i].second;
         numer = kontakty[i].second;
-        wartosc_w_tajtonach += numer % 10 * 100;
-        wartosc_w_tajtonach += (numer % 100 - numer % 10);
-        wartosc_w_tajtonach += (numer % 1000 - numer % 100) / 100;
-        wartosc_w_tajtonach += (numer - numer % 1000);
-        kontakty[i].first = wartosc_w_tajtonach;
+        kontakty[i].first += numer % 10 * 100;
+        kontakty[i].first += (numer % 100 - numer % 10);
+        kontakty[i].first += (numer % 1000 - numer % 100) / 100;
+        kontakty[i].first += (numer - numer % 1000);
     }
     sort(kontakty, kontakty + il_kontaktow);
     for (i = 0; i < il_kontaktow; ++i) {
