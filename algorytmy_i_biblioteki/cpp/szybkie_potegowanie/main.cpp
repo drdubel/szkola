2cde202811aeb5c1b8514c43447cbd093187967a
DrDubel <antek.suszko@gmail.com>
2023-01-27 18:18:34 +0100

    szybkie potęgowanie

--- algorytmy_i_biblioteki/cpp/szybkie_potegowanie/main.cpp
+++ algorytmy_i_biblioteki/cpp/szybkie_potegowanie/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
@@ -9,17 +9,8 @@ ll wynik;
 
 ll pot(ll x, ll n) {
     if (n == 0) return 1;
-    if (n % 2 == 0) return pow(pot(x, n / 2), 2);
-    else return pot(x, n - 1) * x;
-}
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    ll il_poteg, x, n;
-    cin >> il_poteg;
-    for (ll i = 0; i < il_poteg; i++) {
-        cin >> x >> n;
-        cout << pot(x, n) << "\n";
-    }
+    if (n % 2 == 0)
+        return pow(pot(x, n / 2), 2);
+    else
+        return pot(x, n - 1) * x;
 }
