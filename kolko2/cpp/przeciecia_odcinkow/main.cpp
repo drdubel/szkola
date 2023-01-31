e2e5eb227dcb80272304a2cd9543c4105cc8070f
DrDubel <antek.suszko@gmail.com>
2023-01-31 19:57:03 +0100

    .

--- kolko2/cpp/przeciecia_odcinkow/main.cpp
+++ kolko2/cpp/przeciecia_odcinkow/main.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-typedef pair<int, int> pii;
+typedef pair<long long, long long> pllll;
 
-pii p1, p2, p3, p4;
+pllll p1, p2, p3, p4;
 int segNum, i;
 
-bool onSegment(pii p, pii q, pii r) {
+bool onSegment(pllll p, pllll q, pllll r) {
     if (q.first <= max(p.first, r.first) && q.first >= min(p.first, r.first) &&
         q.second <= max(p.second, r.second) &&
         q.second >= min(p.second, r.second))
@@ -16,16 +16,16 @@ bool onSegment(pii p, pii q, pii r) {
     return false;
 }
 
-int orientation(pii p, pii q, pii r) {
-    int val = (q.second - p.second) * (r.first - q.first) -
-              (q.first - p.first) * (r.second - q.second);
+int orientation(pllll p, pllll q, pllll r) {
+    long long val = (q.second - p.second) * (r.first - q.first) -
+                    (q.first - p.first) * (r.second - q.second);
 
     if (val == 0) return 0;
 
     return (val > 0) ? 1 : 2;
 }
 
-bool doIntersect(pii p1, pii p2, pii p3, pii p4) {
+bool doIntersect(pllll p1, pllll p2, pllll p3, pllll p4) {
     int o1 = orientation(p1, p2, p3);
     int o2 = orientation(p1, p2, p4);
     int o3 = orientation(p3, p4, p1);
@@ -40,12 +40,11 @@ bool doIntersect(pii p1, pii p2, pii p3, pii p4) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> segNum;
+    scanf("%d", &segNum);
     for (i = 0; i < segNum; ++i) {
-        cin >> p1.first >> p1.second >> p2.first >> p2.second >> p3.first >>
-            p3.second >> p4.first >> p4.second;
-        doIntersect(p1, p2, p3, p4) ? cout << "TAK\n" : cout << "NIE\n";
+        scanf("%lld%lld%lld%lld%lld%lld%lld%lld", &p1.first, &p1.second,
+              &p2.first, &p2.second, &p3.first, &p3.second, &p4.first,
+              &p4.second);
+        doIntersect(p1, p2, p3, p4) ? printf("TAK\n") : printf("NIE\n");
     }
 }
