b0ee1bb0b6156da46d1a06ae5fe71a0eb2934034
DrDubel <antek.suszko@gmail.com>
2023-02-04 20:12:43 +0100

    tarasy

--- kolko1/cpp/tarasy/main.cpp
+++ kolko1/cpp/tarasy/main.cpp
@@ -7,20 +7,19 @@ const int MAXN = 2e4 + 7;
 int tarasy[MAXN], il_tarasow, il_kredytow, wyn, pocz, koszt, kon, i;
 
 int rozwiaz() {
-    int wynik = 0;
-    int kon = 0;
-    int koszt = 0;
-    for (int pocz = 0; pocz < il_tarasow; pocz++) {
-        while (kon < pocz && koszt > il_kredytow) {
-            koszt -= max(0, tarasy[kon + 1] - tarasy[kon]);
+    kon = 0;
+    koszt = 0;
+    for (pocz = 0; pocz < il_tarasow; pocz++) {
+        while (kon < pocz && il_kredytow < koszt) {
+            il_kredytow += max(0, tarasy[kon + 1] - tarasy[kon]);
             kon++;
         }
         if (koszt <= il_kredytow) {
-            wynik = max(pocz - kon + 1, wynik);
+            wyn = max(pocz - kon + 1, wyn);
         }
-        koszt += max(0, tarasy[pocz + 1] - tarasy[pocz]);
+        il_kredytow -= max(0, tarasy[pocz + 1] - tarasy[pocz]);
     }
-    return wynik;
+    return wyn;
 }
 
 int main() {
@@ -32,5 +31,5 @@ int main() {
     }
     wyn = rozwiaz();
     reverse(tarasy, tarasy + il_tarasow);
-    cout << max(wyn, rozwiaz());
+    cout << rozwiaz();
 }
--- lekcje/cpp/tarasy/main.cpp
+++ lekcje/cpp/tarasy/main.cpp
@@ -7,18 +7,17 @@ const int MAXN = 2e4 + 7;
 int tarasy[MAXN], il_tarasow, il_kredytow, wyn, pocz, koszt, kon, i;
 
 int rozwiaz() {
-    wyn = 0;
     kon = 0;
     koszt = 0;
-    for (int pocz = 0; pocz < il_tarasow; pocz++) {
-        while (kon < pocz && koszt > il_kredytow) {
-            koszt -= max(0, tarasy[kon + 1] - tarasy[kon]);
+    for (pocz = 0; pocz < il_tarasow; pocz++) {
+        while (kon < pocz && il_kredytow < koszt) {
+            il_kredytow += max(0, tarasy[kon + 1] - tarasy[kon]);
             kon++;
         }
         if (koszt <= il_kredytow) {
             wyn = max(pocz - kon + 1, wyn);
         }
-        koszt += max(0, tarasy[pocz + 1] - tarasy[pocz]);
+        il_kredytow -= max(0, tarasy[pocz + 1] - tarasy[pocz]);
     }
     return wyn;
 }
@@ -32,5 +31,5 @@ int main() {
     }
     wyn = rozwiaz();
     reverse(tarasy, tarasy + il_tarasow);
-    cout << max(wyn, rozwiaz());
+    cout << rozwiaz();
 }
