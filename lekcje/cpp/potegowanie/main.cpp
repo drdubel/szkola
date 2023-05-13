634fc04f82386fc99d153b17cd9b95e2340cc8de
DrDubel <antek.suszko@gmail.com>
2023-05-13 14:04:38 +0200

    zadanie potegowanie z lekcji

---
+++ lekcje/cpp/potegowanie/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+typedef long long ll;
+
+using namespace std;
+
+const ll MAXN = 1e6 + 7;
+ll n, a, b;
+ll wynik;
+
+ll pot(ll x, ll n) {
+    x %= MAXN;
+    if (n == 0) return 1;
+    if (n % 2 == 0)
+        return (ll) pow(pot(x, n / 2) % MAXN, 2) % MAXN;
+    else
+        return (pot(x, n - 1) % MAXN * x) % MAXN;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << pot(a, b) << "\n";
+    }
+}
