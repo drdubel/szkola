3e89cb83e0327e2f08f91c086882a61a827d496d
drdubel <antek.suszko@gmail.com>
2025-01-09 21:50:29 +0100

    działa

--- preoi24/smol/dzien3/swiszcze/main.cpp
+++ preoi24/smol/dzien3/swiszcze/main.cpp
@@ -5,30 +5,41 @@
 using namespace std;
 
 constexpr int MAXN = 5e5 + 7;
-pair<int, int> swiszcze[MAXN];
+
+pair<int, int> wej[MAXN], tab[2 * MAXN];
+bool wziete[MAXN];
+vector<int> swiszcze;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, il_grup = 0;
-
+    int n;
     cin >> n;
 
-    for (int i = 0; i < n; ++i) {
-        cin >> swiszcze[i].first >> swiszcze[i].second;
+    for (int i = 1; i <= n; i++) {
+        cin >> wej[i].first >> wej[i].second;
+        wej[i].second = wej[i].first / wej[i].second + 1;
     }
 
-    sort(swiszcze, swiszcze + n, greater<pair<int, int>>());
-
-    double najmlodszy = 1e9 + 7;
-
-    for (int i = 0; i < n; ++i) {
-        if (najmlodszy > (double)swiszcze[i].first) {
-            najmlodszy = swiszcze[i].first / swiszcze[i].second;
-            il_grup++;
-        }
+    for (int i = 1; i <= n; i++) {
+        tab[2 * i - 1] = {wej[i].second, -i};
+        tab[2 * i] = {wej[i].first, i};
     }
 
-    cout << il_grup << '\n';
+    sort(tab + 1, tab + 1 + 2 * n);
+
+    int wynik = 0;
+    for (int i = 1; i <= 2 * n; i++) {
+        if (tab[i].second > 0) {
+            if (wziete[tab[i].second]) continue;
+            wynik++;
+            for (int x : swiszcze) wziete[x] = true;
+
+            swiszcze.clear();
+        } else
+            swiszcze.push_back(-tab[i].second);
+    }
+
+    cout << wynik << '\n';
 }
