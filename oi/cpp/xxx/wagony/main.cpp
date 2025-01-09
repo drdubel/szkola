99ded837b735ed6c2927b73bce16d8aaf142b633
drdubel <antek.suszko@gmail.com>
2025-01-09 21:49:35 +0100

    jeszcze nie działa

---
+++ oi/cpp/xxx/wagony/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, d, a, b;
+
+    cin >> n >> d >> a >> b;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+    }
+
+    int wynik = 0;
+
+    cout << wynik % 1001 << "\n";
+}
